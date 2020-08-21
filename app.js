0b0e9ad0260bbe44a8aa56a49095cddbfc2ae28c
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-21 23:18:54 +0200

    m

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-var express = require(‘express’);
-var port = process.env.PORT || 3000;
-var app = express();
-app.get(‘/’, function (req, res) {
- res.send(JSON.stringify({ Hello: ‘World’}));
+var express=require("express");
+var app=express();
+ var mysql=require('mysql');
+ var bodyParser=require('body-parser');
+
+ app.use(bodyParser.json({type:'application/json'}));
+ app.use(bodyParser.urlencoded({extended:true}));
+ const PORT=8080;
+ var con=mysql.createConnection({
+     host:'localhost',
+     user:'root',
+     password:'',
+     database:'foodorder_db'
+  
+
+ });
+ var server=app.listen(8080,function(){
+    var host=server.address().address
+    var port=server.address().port
+    console.log("start");
 });
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
+con.connect(function(error){
+    if(error) console.log(error);
+    else console.log("Connected");
+})
+app.get('/restaurant',function(req,res){
+    con.query('select * from restaurant',function(error,rows,fields){
+        if(error) console.log(error);
+        else
+        {
+            console.log(rows);
+            res.send(rows);
+        }
+    })
 });
