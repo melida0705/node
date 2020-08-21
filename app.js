387f2454ae354ae1abf4f2f5b9be3658da3218c9
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-21 23:30:22 +0200

    s

--- app.js
+++ app.js
@@ -1,35 +1,67 @@
-var express=require("express");
-var app=express();
- var mysql=require('mysql');
- var bodyParser=require('body-parser');
-
- app.use(bodyParser.json({type:'application/json'}));
- app.use(bodyParser.urlencoded({extended:true}));
- const PORT=8080;
- var con=mysql.createConnection({
-     host:'localhost',
-     user:'root',
-     password:'',
-     database:'foodorder_db'
-  
-
- });
- var server=app.listen(8080,function(){
-    var host=server.address().address
-    var port=server.address().port
-    console.log("start");
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+app.use(express.json());
+const courses = [
+  { id: 1, name: "Algorithms" },
+  { id: 2, name: "Software Engineering" },
+  { id: 3, name: "Human Computer Interaction" }
+];
+app.get("/", function(req, res) {
+  //when we get an http get request to the root/homepage
+  res.send("Hello World");
 });
-con.connect(function(error){
-    if(error) console.log(error);
-    else console.log("Connected");
-})
-app.get('/restaurant',function(req,res){
-    con.query('select * from restaurant',function(error,rows,fields){
-        if(error) console.log(error);
-        else
-        {
-            console.log(rows);
-            res.send(rows);
-        }
-    })
+//when we route to /courses
+app.get("/courses", function(req, res) {
+  res.send(courses); //respond with the array of courses
+});
+//To get a specific course, we need to define a parameter id
+app.get("/courses/:id", function(req, res) {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  //if the course does not exist return status 404 (not found)
+  if (!course)
+      return res
+          .status(404)
+          .send("The course with the given id was not found");
+  //return the object
+  res.send(course);
+});
+//using the http post request we can create a new course
+app.post("/courses", function(req, res) {
+  //create a course object
+  const course = {
+      id: courses.length + 1,
+      name: req.body.name
+  };
+  //add the course to the array
+  courses.push(course);
+  //return the course
+  res.send(course);
+});
+app.put("/courses/:id", function(req, res) {
+  //get the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+      return res
+          .status(404)
+          .send("The course with the given id was not found");
+  //update the course
+  course.name = req.body.name;
+  //return the updated object
+  res.send(course);
+});
+app.put("/courses/:id", function(req, res) {
+  //get the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+      return res
+          .status(404)
+          .send("The course with the given id was not found");
+  //update the course
+  course.name = req.body.name;
+  //returns the updated object
+  res.send(course);
+});
+app.listen(PORT, function() {
+  console.log(`Listening on Port ${PORT}`);
 });
