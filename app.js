68309c71f14dc64f5b6faa0caf1f39411d095f77
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-24 03:55:08 +0200

    emails

--- app.js
+++ app.js
@@ -46,7 +46,13 @@ app.get('/send/:to',function(req,res)
     });
     
     // link="klikni odek";
-  
+  smtpTransport.sendMail({
+    from:'melidaradoncic@hotmail.com',
+        to : req.params.to,
+        subject :`Please confirm your Email account`,
+        html : `Hello,<br> Please Click on the link to verify your email.<br><a href="${link}">Click here to verify</a>`
+    
+  })
   // smtpTransport.sendMail(
   //     {
   //       from:"melidaradoncic@hotmail.com",
