4fe6497dd43da240ed0703de5500ffe97483117e
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-24 09:04:25 +0200

    c

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.post('/leavereview/:customerid/:restaurantid:/:mark/:review',function(req,re
 app.get('/login/:username/:password',function(req,res){
   var status=0;
   
-    con.query("select * from users where users.username='"+req.params.username+"' and users.password='"+md5(req.params.password)+"'",function(error,rows,fields){
+    con.query("select * from users where users.username='"+req.params.username+"' and users.password='"+md5.hex_md5(req.params.password)+"'",function(error,rows,fields){
         if(error) console.log(error);
         else
         {
