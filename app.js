af4c7b945e134a49e864554b3a335c4f54b423eb
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-24 03:45:43 +0200

    c

--- app.js
+++ app.js
@@ -38,9 +38,8 @@ app.get('/send/:to',function(req,res)
    // host=req.get('host');
     //link="http://"+req.get('host')+"/verify?id="+rand;
     const smtpTransport=nodemailer.createTransport({
-      host:'smtp.live.com',
-      port:25,
-      
+      service:hotmail,
+    
       auth:{
         user:"melidaradoncic@hotmail.com",
         password:"CocaColaMalboro123"
@@ -49,15 +48,15 @@ app.get('/send/:to',function(req,res)
     
     link="klikni odek";
   
-  smtpTransport.sendMail(
-      {
-        from:"melidaradoncic@hotmail.com",
-        to : req.params.to,
-        subject : "Please confirm your Email account",
-        html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+  // smtpTransport.sendMail(
+  //     {
+  //       from:"melidaradoncic@hotmail.com",
+  //       to : req.params.to,
+  //       subject : "Please confirm your Email account",
+  //       html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
     
-      }).catch(err);
-      var mail=req.params.to;
+  //     }).catch(err);
+  //     var mail=req.params.to;
       res.send(mail)
 });
 // app.get('/verify',function(req,res){
