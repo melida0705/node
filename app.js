033e3dfea0b884ad6947f40e9b56be37f2d7b476
Melida Radoncic <melidaradoncic@hotmail.com>
2020-08-24 02:51:33 +0200

    c

--- app.js
+++ app.js
@@ -22,59 +22,59 @@ const courses = [
   { id: 2, name: "Software Engineering" },
   { id: 3, name: "Human Computer Interaction" }
 ];
-var rand,mailOptions,host,link;
-var smtpTransport=nodemailer.createTransport("SMTP",{
-  service:"Hotmail",
-  auth:{
-    user:"melidaradoncic@hotmail.com",
-    password:"CocaColaMalboro123"
-  }
+// var rand,mailOptions,host,link;
+// var smtpTransport=nodemailer.createTransport("SMTP",{
+//   service:"Hotmail",
+//   auth:{
+//     user:"melidaradoncic@hotmail.com",
+//     password:"CocaColaMalboro123"
+//   }
   
-})
+// })
 
-app.get('/send/:to',function(req,res)
-{
- // rand=Math.floor((Math.random() * 100) + 54);
-   // host=req.get('host');
-    //link="http://"+req.get('host')+"/verify?id="+rand;
-    link="klikni odek"
-  mailOptions={
-    from:"melidaradoncic@hotmail.com",
-    to : req.params.to,
-    subject : "Please confirm your Email account",
-    html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
-}
-smtpTransport.sendMail(mailOptions, function(error, response){
-  if(error){
-         console.log(error);
-     res.send("error");
-  }else{
-         console.log("Message sent: " + response.message);
-     res.send("sent");
-      }
-});
-});
-app.get('/verify',function(req,res){
-  console.log(req.protocol+":/"+req.get('host'));
-  if((req.protocol+"://"+req.get('host'))==("http://"+host))
-  {
-      console.log("Domain is matched. Information is from Authentic email");
-      if(req.body.id==rand)
-      {
-          console.log("email is verified");
-          res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-      }
-      else
-      {
-          console.log("email is not verified");
-          res.end("<h1>Bad Request</h1>");
-      }
-  }
-  else
-  {
-      res.end("<h1>Request is from unknown source");
-  }
-  });
+// app.get('/send/:to',function(req,res)
+// {
+//  // rand=Math.floor((Math.random() * 100) + 54);
+//    // host=req.get('host');
+//     //link="http://"+req.get('host')+"/verify?id="+rand;
+//     link="klikni odek"
+//   mailOptions={
+//     from:"melidaradoncic@hotmail.com",
+//     to : req.params.to,
+//     subject : "Please confirm your Email account",
+//     html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>" 
+// }
+// smtpTransport.sendMail(mailOptions, function(error, response){
+//   if(error){
+//          console.log(error);
+//      res.send("error");
+//   }else{
+//          console.log("Message sent: " + response.message);
+//      res.send("sent");
+//       }
+// });
+// });
+// app.get('/verify',function(req,res){
+//   console.log(req.protocol+":/"+req.get('host'));
+//   if((req.protocol+"://"+req.get('host'))==("http://"+host))
+//   {
+//       console.log("Domain is matched. Information is from Authentic email");
+//       if(req.body.id==rand)
+//       {
+//           console.log("email is verified");
+//           res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
+//       }
+//       else
+//       {
+//           console.log("email is not verified");
+//           res.end("<h1>Bad Request</h1>");
+//       }
+//   }
+//   else
+//   {
+//       res.end("<h1>Request is from unknown source");
+//   }
+//   });
   
 app.get('/restaurant',function(req,res){
     con.query('select * from restaurant',function(error,rows,fields){
