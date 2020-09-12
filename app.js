28ea853c209ef59c35aa194d215b4359012e645d
Melida Radoncic <melidaradoncic@hotmail.com>
2020-09-13 00:58:27 +0200

    d

--- app.js
+++ app.js
@@ -586,7 +586,7 @@ app.get('/seller-allorder/:restaurantid',function(req,res,next)
 });
 app.get('/customer-allorder/:customerid',function(req,res,next)
 {
-  con.query("SELECT  * FROM orders where orders.order_customer_username='"+req.params.customerid+"' and order_shipped='0' and order_preparation='0' and order_delivered='0'" ,function(error,rows,fields){
+  con.query("SELECT  * FROM orders INNER JOIN order_details ON orders.order_id=order_details.order_id INNER JOIN food ON food.food_id=order_details.order_food_id where orders.order_customer_username='"+req.params.customerid+"' and order_shipped='0' and order_preparation='0' and order_delivered='0'" ,function(error,rows,fields){
       if(error) console.log(error);
       else
       {   console.log("Radim");
