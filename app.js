b66a85b91642de49703ce0334789e8115e6379f5
Melida Radoncic <melidaradoncic@hotmail.com>
2020-10-03 03:06:34 +0200

    d

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/send/:to/:username/:password',function (req,res)
     from: process.env.HOTMAIL_USER,
         to : req.params.to,
         subject :'Please confirm your Email account',
-        html:`Click on this link to verify your email address: <a href="${link}>${link}</a>`
+        html:`Click on this link to verify your email address: <a href="${url}>${url}</a>`
 
     })
      
